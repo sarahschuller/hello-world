5f9bd148be439bf3d27ba3d667a0190a97da8f4b
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-01 19:34:23 -0400

    import button component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
  constructor(props) {

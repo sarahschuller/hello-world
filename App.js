51896582af575884decf22d3b6e44cfc541c3b82
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-01 19:32:18 -0400

    add code for text input

--- App.js
+++ App.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 
-export default class HelloWorld extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{width: 160, height: 60, backgroundColor: 'purple'}}></View>
-        <View style={styles.box2}></View>
-        <View style={styles.box3}></View>
-      </View>
-    );
-  }
-}
+export default class App extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = { text: '' };
+ }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  box1: {
-    flex: 1,
-    backgroundColor: 'purple'
-  },
-  box2: {
-    flex:1,
-    backgroundColor: 'orange'
-  },
-  box3: {
-    flex: 1,
-    backgroundColor: 'yellow'
-  }
-});
+ render() {
+   return (
+     <View style={{flex:1, justifyContent:'center'}}>
+       <TextInput
+         style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+         onChangeText={(text) => this.setState({text})}
+         value={this.state.text}
+         placeholder='Type here ...'
+       />
+       <Text>You wrote: {this.state.text}</Text>
+     </View>
+   );
+ }
+}

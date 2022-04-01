dc3bf82369c0596c562fa9461a5809a59651b392
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-01 19:24:35 -0400

    updating styling

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default class HelloWorld extends Component {
   render() {
     return (
-      <View></View>
+      <View style={styles.container}>
+        <View style={{width: 60, height: 60, backgroundColor: 'purple'}}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-})
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  box1: {
+    flex: 1,
+    backgroundColor: 'purple'
+  },
+  box2: {
+    flex:1,
+    backgroundColor: 'orange'
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  }
+});

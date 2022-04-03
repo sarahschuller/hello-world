8c9b45ae80f63c229d061212ca774c76ddaddd59
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-03 14:33:02 -0400

    add componountDidMount function and onSend function

--- components/Chat.js
+++ components/Chat.js
@@ -11,7 +11,30 @@ export class Chat extends React.Component {
       messages: [],
     }
   }
-  
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  } 
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
   render() {
 
     //User desired name appears at top of screen

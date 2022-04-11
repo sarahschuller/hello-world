af30eb91ffdd07ff08392b977f16b5bf4773945e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-04-11 16:14:37 -0400

    add renderCustomView function for map locations in chat

--- components/Chat.js
+++ components/Chat.js
@@ -232,6 +232,28 @@ export default class Chat extends React.Component {
     return <CustomActions {...props} />;
   };
 
+  // Render Custom View to show map location
+  renderCustomView (props) {
+    const { currentMessage} = props;
+    if (currentMessage.location) {
+      return (
+          <MapView
+            style={{width: 150,
+              height: 100,
+              borderRadius: 13,
+              margin: 3}}
+            region={{
+              latitude: currentMessage.location.latitude,
+              longitude: currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+      );
+    }
+    return null;
+  }
+
   render() {
     //Updates name on chat screen
     let name = this.props.route.params.name;
